126e109bfc8acd1f33f5e9da450d21dd1ef7b752
wilkinsleung0712 <wilkinsweiqiangliang@gmail.com>
2017-01-08 23:23:49 +1100

    adding CatItem functionality to DAO and Service level.

--- src/main/java/com/taotao/rest/controller/CatItemController.java
+++ src/main/java/com/taotao/rest/controller/CatItemController.java
@@ -1,15 +1,27 @@
 package com.taotao.rest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.taotao.rest.pojo.CatResult;
+import com.taotao.rest.service.CatItemService;
 
 @Controller
-@RequestMapping("/rest/itemcat")
+@RequestMapping("/itemcat")
 public class CatItemController {
 
-	@RequestMapping("/all")
-	public Object getItemCatList(String callback) {
+    @Autowired
+    private CatItemService catItemService;
 
-		return null;
-	}
+    @RequestMapping("/all")
+    @ResponseBody
+    public MappingJacksonValue getItemCatList(String callback) {
+        CatResult list = catItemService.getItemCatList();
+        MappingJacksonValue result = new MappingJacksonValue(list);
+        result.setJsonpFunction(callback);
+        return result;
+    }
 }
--- src/main/java/com/taotao/rest/service/impl/CatItemServiceImpl.java
+++ src/main/java/com/taotao/rest/service/impl/CatItemServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import com.taotao.mapper.TbItemCatMapper;
 import com.taotao.pojo.CatNode;
@@ -13,50 +15,61 @@ import com.taotao.pojo.TbItemCatExample.Criteria;
 import com.taotao.rest.pojo.CatResult;
 import com.taotao.rest.service.CatItemService;
 
+@Service
 public class CatItemServiceImpl implements CatItemService {
 
-	@Autowired
-	private TbItemCatMapper itemCatMapper;
+    @Value("${MAX_CAT_ITEM_COUNTER}")
+    private int MAX_CAT_ITEM_COUNTER;
+    
+    @Autowired
+    private TbItemCatMapper itemCatMapper;
 
-	@Override
-	public CatResult getItemCatList() {
-		CatResult result = new CatResult();
-		result.setData(getItemCatListByParentId(0));
-		return result;
-	}
+    @Override
+    public CatResult getItemCatList() {
+        CatResult result = new CatResult();
+        result.setData(getItemCatListByParentId(0));
+        return result;
+    }
 
-	/**
-	 * 查询分类列表
-	 * 
-	 * @return
-	 */
-	private List<?> getItemCatListByParentId(long parentId) {
-		// 创建查询条件
-		TbItemCatExample example = new TbItemCatExample();
-		Criteria criteria = example.createCriteria();
-		criteria.andParentIdEqualTo(parentId);
-		// 执行查询
-		List<TbItemCat> list = itemCatMapper.selectByExample(example);
-
-		// 返回值list
-		List<String> resultList = new ArrayList<>();
-
-		for (TbItemCat tbItemCat : list) {
-			if (tbItemCat.getIsParent()) {
-				CatNode catNode = new CatNode();
-				if (parentId == 0) {
-					catNode.setName(
-							"<a href='/products/" + tbItemCat.getId() + ".html'>" + tbItemCat.getName() + "</a>");
-				} else {
-					catNode.setName(tbItemCat.getName());
-				}
-				catNode.setUri("/product/" + tbItemCat.getId() + ".html");
-				catNode.setItem(getItemCatListByParentId(tbItemCat.getId()));
-			} else {
-				resultList.add("/products/" + tbItemCat.getId() + ".html|" + tbItemCat.getName());
-			}
-		}
-		return resultList;
-	}
+    /**
+     * 查询分类列表
+     * 
+     * @return
+     */
+    private List<?> getItemCatListByParentId(long parentId) {
+        // 创建查询条件
+        TbItemCatExample example = new TbItemCatExample();
+        Criteria criteria = example.createCriteria();
+        criteria.andParentIdEqualTo(parentId);
+        // 执行查询
+        List<TbItemCat> list = itemCatMapper.selectByExample(example);
+        // 设置COUNTER统计类别数目
+        int counter = 0;
+        // 返回值list
+        List<Object> resultList = new ArrayList<>();
+        for (TbItemCat tbItemCat : list) {
+            if (tbItemCat.getIsParent()) {
+                CatNode catNode = new CatNode();
+                if (parentId == 0) {
+                    catNode.setName("<a href='/products/" + tbItemCat.getId()
+                            + ".html'>" + tbItemCat.getName() + "</a>");
+                    counter++;
+                } else {
+                    catNode.setName(tbItemCat.getName());
+                }
+                //如果类别太多，只输出最大类别数目
+                if (counter > MAX_CAT_ITEM_COUNTER) {
+                    break;
+                }
+                catNode.setUri("/product/" + tbItemCat.getId() + ".html");
+                catNode.setItem(getItemCatListByParentId(tbItemCat.getId()));
+                resultList.add(catNode);
+            } else {
+                resultList.add("/products/" + tbItemCat.getId() + ".html|"
+                        + tbItemCat.getName());
+            }
+        }
+        return resultList;
+    }
 
 }
