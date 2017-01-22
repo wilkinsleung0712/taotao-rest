187d44bf56a472394de4d858a5604ce574684e33
wilkinsleung0712 <wilkinsweiqiangliang@gmail.com>
2017-01-22 22:58:54 +1100

    implement redius feature for application to read and save content json data into redius for future accessing.

--- src/main/java/com/taotao/rest/service/impl/ContentServiceImpl.java
+++ src/main/java/com/taotao/rest/service/impl/ContentServiceImpl.java
@@ -1,15 +1,20 @@
 package com.taotao.rest.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
 import com.taotao.pojo.TbContentExample;
 import com.taotao.pojo.TbContentExample.Criteria;
+import com.taotao.rest.dao.JediusClientSingle;
 import com.taotao.rest.service.ContentService;
+import com.taotao.util.JsonUtils;
 
 @Service
 public class ContentServiceImpl implements ContentService {
@@ -17,14 +22,41 @@ public class ContentServiceImpl implements ContentService {
     @Autowired
     private TbContentMapper contentMapper;
 
+    @Autowired
+    private JediusClientSingle jediusClientSingle;
+
+    @Value("${INDEX_CONTENT_REDIS_KEY}")
+    private String INDEX_CONTENT_REDIS_KEY;
+
     @Override
     public List<TbContent> getContentByCategoryId(long categoryId) {
+        String rediusResult = jediusClientSingle.hget(INDEX_CONTENT_REDIS_KEY,
+                String.valueOf(categoryId));
+        List<TbContent> list = new ArrayList<>();
+        try {
+            if (!StringUtils.isBlank(rediusResult)) {
+                list = JsonUtils.jsonToList(rediusResult, TbContent.class);
+                return list;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         // 根据内容分类id查询内容列表
         TbContentExample example = new TbContentExample();
         Criteria criteria = example.createCriteria();
         criteria.andCategoryIdEqualTo(categoryId);
         // 根据内容分类id查询内容列表
-        List<TbContent> list = contentMapper.selectByExampleWithBLOBs(example);
+        list = contentMapper.selectByExampleWithBLOBs(example);
+        // 向缓存中添加内容
+        try {
+            // 把list转换成字符串
+            String listJsonData = JsonUtils.objectToJson(list);
+            jediusClientSingle.hset(INDEX_CONTENT_REDIS_KEY,
+                    String.valueOf(categoryId), listJsonData);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return list;
     }
